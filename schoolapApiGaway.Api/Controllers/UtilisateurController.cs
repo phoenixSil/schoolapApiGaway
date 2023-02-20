b3cbba7c42a5b7af5e68256d597bfa400ee0e89e
Sillery TALLA Philinte <tsillery@ymail.com>
2023-02-19 19:42:47 -0500

    ajout de controller

---
+++ schoolapApiGaway.Api/Controllers/UtilisateurController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace schoolapApiGaway.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UtilisateurController : ControllerBase
+    {
+        public UtilisateurController()
+        {
+
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<string>> LireTousLesUtilisateurs()
+        {
+            return "allelouia";
+        }
+    }
+}
